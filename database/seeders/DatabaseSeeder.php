a7c957cc78355fc6b6f723709d5d53d1e940c4a6
mdrdani <sudokill3004@gmail.com>
2022-04-17 04:07:07 +0000

    User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
