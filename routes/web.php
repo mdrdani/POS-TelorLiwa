bf344cffba4e2230f5179510ae84eef71d55efdf
mdrdani <sudokill3004@gmail.com>
2022-05-10 04:28:12 +0000

    Controller Permissions

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,9 @@ Route::prefix('apps')->group(function() {
     Route::group(['middleware' => ['auth']], function() {
         // route dashboard
         Route::get('dashboard', App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
+
+        // route permissions
+        Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
+            ->middleware('permission:permissions.index');
     });
 });
